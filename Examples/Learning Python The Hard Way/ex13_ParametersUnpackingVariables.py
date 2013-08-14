75ea000cb488893621fc2cc2fc967e1c1c489723
KellyChan <kwailamchan@hotmail.com>
2013-08-14 13:36:28 +0800

    Learning python the hard way updated

--- Examples/Learning Python The Hard Way/ex13_ParametersUnpackingVariables.py
+++ Examples/Learning Python The Hard Way/ex13_ParametersUnpackingVariables.py
@@ -9,14 +9,6 @@ Ex13: Parameters, Unpacking, Variables
 
 '''
 
-#from sys import argv
-
-#script, first, second, third = argv
-    
-#print("The script is called:", script)
-#print("Your first variable is:", first)
-#print("Your second variable is:", second)
-#print("Your third variable is:", third)
 
 import sys
 
