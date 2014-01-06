33482e0504c26bff0442bd2e0b9025405f28d596
KellyChan <kwailamchan@hotmail.com>
2014-01-06 19:17:56 +0800

    Update bin.py

--- Examples/Petites/bin.py
+++ Examples/Petites/bin.py
@@ -3,3 +3,11 @@ print bin(2)
 print bin(3)
 print bin(4)
 print bin(5)
+
+
+print int("1",2)
+print int("10",2)
+print int("111",2)
+print int("0b100",2)
+print int(bin(5),2)
+print int("11001001",2)
