6885a4ad189923487dfe2236e9075aca3d465654
KellyChan <kwailamchan@hotmail.com>
2014-01-06 10:17:45 +0800

    Create comma.py

---
+++ Examples/Petites/comma.py
@@ -0,0 +1,9 @@
+s = "A bird in the hand..."
+
+# Add your for loop
+for word in s:
+    if word == 'A' or word == 'a':
+        print 'X',
+    else:
+        print word, # , for printing in a line, not create a new line
+
