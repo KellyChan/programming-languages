48975543eb07df0764d82d7dbc95d7d81a6327ad
KellyChan <kwailamchan@hotmail.com>
2016-09-11 01:01:33 +0800

    integrated cuda into cpp

---
+++ cpp/cuda/src/demos/segfault.cu
@@ -0,0 +1,17 @@
+#include "../utils/utils.h"
+
+__global__
+void myKernel(int *in) {
+    in[threadIdx.x] += 1;
+}
+
+
+int main(void) {
+    int *dIn;
+    checkCudaErrors(cudaMalloc(&dIn, sizeof(int)));
+
+    myKernel<<<1,2>>>(dIn);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    return 0;
+}
---
+++ cpp/cuda/src/demos/summation.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <limits>
+
+
+int main(void) {
+
+    const int N = 100000000;
+
+    std::vector<float> numbers(N);
+
+    for (int i = 0; i < N; ++i)
+      numbers[i] = ((double)rand() / (double)std::numeric_limits<int>::max());
+
+    float sum = 0.f;
+    for (int i = 0; i < N; ++i)
+      sum += numbers[i];
+    std::cout << "sum float: " << std::setprecision(20) << sum << std::endl;
+
+    double sum_d = 0.;
+    for (int i = 0; i < N; ++i)
+      sum_d += numbers[i];
+    std::cout << "sum double: " << std::setprecision(20) << sum_d << std::endl;
+
+    std::sort(numbers.begin(), numbers.end());
+    std::cout << "\nAfter sorting\n";
+
+    sum = 0.f;
+    for (int i = 0; i < N; ++i)
+      sum += numbers[i];
+    std::cout << "sum float: " << std::setprecision(20) << sum << std::endl;
+
+    sum_d = 0.;
+    for (int i = 0; i < N; ++i)
+      sum_d += numbers[i];
+    std::cout << "sum double: " << std::setprecision(20) << sum_d << std::endl;
+
+    return 0;
+
+}
