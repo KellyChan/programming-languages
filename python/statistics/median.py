19e1215d8b5af00be775d0667383aeba3785fb1c
Kelly <kwailamchan@hotmail.com>
2017-09-04 14:06:38 +0800

    [master] removed statistics.

--- python/statistics/median.py
+++
@@ -1,8 +0,0 @@
-def median(lst):
-    lst.sort()
-    if len(lst) % 2 != 0: #Odd number of terms
-        return lst[int((len(lst)-1)/2)]
-    else: #Even number of terms
-        term1 = lst[int((len(lst)/2)-1)]
-        term2 = lst[int((len(lst)/2))]
-        return (term1 + term2)/2.0
--- python/statistics/mode.py
+++
@@ -1,8 +0,0 @@
-def mode(data):
-    modecnt = 0
-    for i in range(len(data)):
-        imode = data.count(data[i])
-        if imode > modecnt:
-            modecnt = imode
-            mode = data[i]
-    return mode
